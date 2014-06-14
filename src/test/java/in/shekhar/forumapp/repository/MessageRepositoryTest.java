22f4f9c86a9d69eb56efdb76ff0f477044b5333f
Shekhar Gulati <shekhargulati84@gmail.com>
2014-06-15 02:58:04 +0530

    added another assertion

--- src/test/java/in/shekhar/forumapp/repository/MessageRepositoryTest.java
+++ src/test/java/in/shekhar/forumapp/repository/MessageRepositoryTest.java
@@ -33,8 +33,8 @@ public class MessageRepositoryTest {
 		
 		messageRepository.save(message);
 		
-		List<Message> messages = messageRepository.findByMessageAuthor("test_user");
-		
+		List<Message> messages = messageRepository.findByMessageAuthor("test_user");		
+		assertNotNull(messages);
 		assertFalse(messages.isEmpty());
 		assertEquals(1, messages.size());
 	}
